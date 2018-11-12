f7cded44a599668d85cdfa3a8797b44846a26845
Konstantin Malyshenko <Konstantin_Malyshenko@epam.com>
2018-11-13 00:21:32 +0300

    hw5 allure

---
+++ src/test/java/hw5/SelenideDatesPageTest.java
@@ -0,0 +1,76 @@
+package hw5;
+
+import base.SelenideTestBase;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import listeners.AllureAttachmentListener;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pageObjects.SelenideDates;
+import pageObjects.SelenideHomePage;
+
+import static com.codeborne.selenide.Selenide.page;
+import static enums.PageTitles.DATES;
+import static enums.PageTitles.HOME_PAGE;
+import static enums.Users.PITER_CHALOVSKII;
+
+@Feature("Smoke tests")
+@Story("Dates Page testing")
+@Listeners(AllureAttachmentListener.class)
+public class SelenideDatesPageTest extends SelenideTestBase {
+
+    private SelenideHomePage selenideHomePage;
+    private SelenideDates selenideDates;
+
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() {
+        selenideHomePage = page(SelenideHomePage.class);
+        selenideDates = page(SelenideDates.class);
+    }
+
+    @Test
+    public void Test() {
+
+        //1. Open URL
+        selenideHomePage.openPage();
+
+        //2.  Assert Browser title
+        selenideHomePage.checkPageTitle(HOME_PAGE);
+
+        //3. Perform login
+        selenideHomePage.login(PITER_CHALOVSKII);
+
+        //4. Assert UserCredentials name in the left-top side of screen that user is loggined
+        selenideHomePage.checkUserName();
+
+        //5. Open through the header menu Service -> Dates Page
+        selenideHomePage.serviceDropDownListClick();
+        selenideHomePage.openDatesPage();
+        selenideHomePage.checkPageTitle(DATES);
+
+        //6. Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most rigth position
+        selenideDates.moveSliders(0, 100);
+
+        //7. Assert that for "From" and "To" sliders there are logs rows with corresponding values
+        selenideDates.checkSliderLogValues(0, 100);
+
+        //8. Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most left position.
+        selenideDates.moveSliders(0, 0);
+
+        //9. Assert that for "From" and "To" sliders there are logs rows with corresponding values
+        selenideDates.checkSliderLogValues(0, 0);
+
+        //10. Using drag-and-drop set Range sliders. left sliders - the most rigth position, right slider - the most rigth position.
+        selenideDates.moveSliders(100, 100);
+
+        //11. Assert that for "From" and "To" sliders there are logs rows with corresponding values
+        selenideDates.checkSliderLogValues(100, 100);
+
+        //12. Using drag-and-drop set Range sliders.
+        selenideDates.moveSliders(30, 70);
+
+        //13. Assert that for "From" and "To" sliders there are logs rows with corresponding values
+        selenideDates.checkSliderLogValues(30, 70);
+    }
+}
---
+++ src/test/java/hw5/SelenideServicePageTest2.java
@@ -0,0 +1,105 @@
+package hw5;
+
+import base.SelenideTestBase;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import listeners.AllureAttachmentListener;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pageObjects.SelenideDifferentElements;
+import pageObjects.SelenideHomePage;
+
+import static com.codeborne.selenide.Selenide.page;
+import static enums.CheckboxItems.WATER;
+import static enums.CheckboxItems.WIND;
+import static enums.DropDownMenuItems.YELLOW;
+import static enums.PageTitles.DIFFERENT_ELEMENTS;
+import static enums.PageTitles.HOME_PAGE;
+import static enums.RadioButtonItems.SELEN;
+import static enums.Users.PITER_CHALOVSKII;
+
+
+@Feature("Smoke tests")
+@Story("Service Page Testing")
+@Listeners(AllureAttachmentListener.class)
+public class SelenideServicePageTest2 extends SelenideTestBase {
+
+    private SelenideHomePage selenideHomePage;
+    private SelenideDifferentElements selenideDifferentElements;
+
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() {
+        selenideHomePage = page(SelenideHomePage.class);
+        selenideDifferentElements = page(SelenideDifferentElements.class);
+
+    }
+
+    @Test
+    public void Test() {
+
+        //1. Open URL
+        selenideHomePage.openPage();
+
+        //2.  Assert Browser title
+        selenideHomePage.checkPageTitle(HOME_PAGE);
+
+        //3. Perform login
+        selenideHomePage.login(PITER_CHALOVSKII);
+
+        //4. Assert UserCredentials name in the left-top side of screen that user is loggined
+        selenideHomePage.checkUserName();
+
+        //5. Click on "Service" subcategory in the header and check that drop down contains options
+        selenideHomePage.serviceDropDownListClick();
+        selenideHomePage.checkServiceDropdownMenuSubcategory();
+
+
+        //6. Click on Service subcategory in the left section and check that drop down contains options
+        selenideHomePage.setServiceLeftMenuClick();
+        selenideHomePage.checkLeftServiceMenuItems();
+
+        //7. Open through the header menu Service -> Different Elements Page
+        selenideHomePage.serviceDropDownListClick();
+        selenideHomePage.openDifferenElementsPage();
+        selenideDifferentElements.checkTitle(DIFFERENT_ELEMENTS);
+
+
+        //8. Check interface on Different elements page, it contains all needed elements
+        selenideDifferentElements.checkCheckBoxItems();
+        selenideDifferentElements.checkDropdownButton();
+        selenideDifferentElements.checkButtons();
+        selenideDifferentElements.checkRadioElements();
+
+        //9. Assert that there is Right Section
+        selenideDifferentElements.checkRightSection();
+
+        //10. Assert that there is Left Section
+        selenideDifferentElements.checkLeftSection();
+
+        //11. Select checkboxes
+        selenideDifferentElements.selectCheckboxes(WATER, WIND);
+
+        //12. Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox. 
+        selenideDifferentElements.checkCheckboxesLogTrue(WATER, WIND);
+
+        //13. Select radio
+        selenideDifferentElements.selectRadioElement(SELEN);
+
+        //14. Assert that for radiobutton there is a log row and value is corresponded to the status of radiobutton. 
+        selenideDifferentElements.checkRadioButton(SELEN);
+
+        //15. Select in dropdown
+        selenideDifferentElements.selectDropdownElement(YELLOW);
+
+        //16. Assert that for dropdown there is a log row and value is corresponded to the selected value. 
+        selenideDifferentElements.checkColorLog(YELLOW);
+
+        //17. Unselect and assert checkboxes
+        selenideDifferentElements.selectCheckboxes(WATER, WIND);
+
+        //18. Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox. 
+        selenideDifferentElements.checkCheckboxesLogFalse(WATER, WIND);
+
+    }
+}
