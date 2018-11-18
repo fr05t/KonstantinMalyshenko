dcb79cf93ae67eb4821dea91f6133b915e6a6520
Konstantin Malyshenko <Konstantin_Malyshenko@epam.com>
2018-11-18 22:28:08 +0300

    listeners

--- src/main/java/listeners/AllureAttachmentListener.java
+++ src/main/java/listeners/AllureAttachmentListener.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class AllureAttachmentListener extends TestListenerAdapter {
 
-    @Attachment(value = "Attachment from {method}:  {0}", type = "image/png")
+    @Attachment(value = "Attachment:  {0}", type = "image/png")
     public byte[] makeScreenshot() {
         byte[] array = {1};
         try {
