c6f9a7e2c8e490c0a19cb5e86d213b987ca9eb36
mkl <aizzzek@gmail.com>
2018-11-19 12:07:50 +0300

    hw4.branch

---
+++ src/test/java/mobile/hw4/MobileTest.java
@@ -0,0 +1,69 @@
+package mobile.hw4;
+
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import setup.Driver;
+
+import static enums.mobile.FieldTitles.CONTACT_NAME;
+import static enums.mobile.FieldTitles.CONTACT_PHONE;
+import static java.lang.System.setProperty;
+
+public class MobileTest extends Driver {
+
+    protected MobileTest() throws Exception {
+        super();
+
+    }
+
+    @BeforeSuite(description = "Set property")
+    public void beforeSuite() {
+        setProperty("webdriver.chrome.driverSingle", "src\\main\\resources\\mobdrv\\mchromedriver.exe");
+    }
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        prepareDriver();
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        driverSingle.quit();
+    }
+
+    @Test(description = "Simple native tests", groups = "native")
+    public void SimplestTest() {
+
+        String app_package_name = "com.example.android.contactmanager:id/";
+
+        //=============================Elements========================
+        By add_btn = By.id(app_package_name + "addContactButton");
+
+        By contactNameTitle = By.id("Contact Name");
+        By contactName = By.xpath("//android.widget.TextView[@content-desc=\"Contact Name\"]\n");
+
+        By contactPhoneTitle = By.id("Contact Phone");
+        By contactPhone = By.xpath("//android.widget.TextView[@content-desc=\"Contact Phone\"]\n");
+
+        //============================Methods==========================
+        driverSingle.findElement(add_btn).click();
+
+
+        //========================Asserts===============================
+        Assert.assertTrue(driverSingle.findElement(contactName).isDisplayed());
+        Assert.assertEquals(driverSingle.findElement(contactNameTitle).getText(), CONTACT_NAME.getTitle());
+        Assert.assertTrue(driverSingle.findElement(contactPhone).isDisplayed());
+        Assert.assertEquals(driverSingle.findElement(contactPhoneTitle).getText(), CONTACT_PHONE.getTitle());
+
+        // Check keyboard is displayed
+        try {
+            driverSingle.hideKeyboard();
+        } catch (Exception e) {
+            Assert.assertTrue(false);
+        }
+    }
+}
